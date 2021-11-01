a11c85488abb6220078b9eddf3cf2bb08b1d0172
benjaminrhansen <benjamminhansen@gmail.com>
2021-11-01 19:39:17 -0400

    All possible ticker symbols available

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 # File:		Gather-Stock-Data
 # %% Imports
+from dotenv.main import dotenv_values
+from dotenv import load_dotenv
 import requests
+import orjson
 
+load_dotenv()
+config = dotenv_values(".env")
 
 # %% Get all possible tickers with financial statements available on FMP
-requ
-# %%
+All_Tickers_URL = f"https://financialmodelingprep.com/api/v3/financial-statement-symbol-lists?apikey={config['API_KEY']}"
+response = requests.get(All_Tickers_URL)
+# get all ticker symbols!
+# get from the response text the JSON data in list form
+All_Tickers = orjson.loads(response.text)
+
+# %% 
